b02da83ec0ecb47a4f4da974609ddedbc385da89
Juan david <juandavidx40@gmail.com>
2025-05-17 23:41:20 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let section = document.querySelector("#prueba");
+let section1 = document.querySelector("#prueba");
+let section2 = document.querySelector("#prueba");
+
+section1.addEventListener("check", (
+
+))
+
