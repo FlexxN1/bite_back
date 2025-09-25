cf8833d0b46ff70bfc457747511c6dc81cbe8761
Juan david <juandavidx40@gmail.com>
2025-09-25 15:33:33 -0500

    changes news

---
+++ src/components/MyOrder.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import OrderItem from './OrderItem';
+import '../style/MyOrder.scss';
+import iconArrow from '@assets/flechita.svg';
+import AppContext from '@context/AppContext';
+import Checkout from '@pages/Checkout';
+
+
+const MyOrder = ({ toggleOrders, setToggleOrders }) => {
+    /*const sumTotal = () => {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+        const sum = state.cart.reduce(reducer, 0);
+        return sum;
+    }*/
+    const { state } = useContext(AppContext);
+
+    return (
+        <aside className="MyOrder">
+            <div className="title-containerOrder"
+                onClick={() => setToggleOrders(!toggleOrders)}
+            >
+                <img src={iconArrow} alt="arrow" />
+                <p className="title1">My order</p>
+            </div>
+            <div className="my-order-content">
+                {state.cart.map((product) => (
+                    <OrderItem product={product} key={`orderItem-${product.id}`} />
+                ))}
+            </div>
+            <div className="order">
+                <p>
+                    <span>Total</span>
+                </p>
+                <p>${state.total}</p>
+            </div>
+            <button>
+                <Link to="/checkout" className="primary-button">Checkou</Link>
+            </button>
+        </aside>
+    );
+}
+
+export default MyOrder;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,23 +7,32 @@ import Home from "@pages/Home";
 import Users from "@pages/Users";
 import Products from "@pages/Products";
 import Perfil from "@pages/Perfil";
+import Navbar from '@components/Navbar';
+import useInitialState from '@hooks/useInitialState';
+import Checkout from '../pages/Checkout';
+import AppContext from '@context/AppContext';
 
 import "../index.scss";
 
+
 export default function App() {
+  const initialState = useInitialState();
   return (
-    <div className="app-container">
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/perfil" element={<Perfil />} />
-          <Route path="/products" element={<Products />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <AppContext.Provider value={initialState}>
+      <div className="app-container">
+        <main>
+          <Routes>
+            <Route path="/" element={<><Navbar/><Home/></>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Registro />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products" element={<><Navbar /><Products /></>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AppContext.Provider>
   );
 }
