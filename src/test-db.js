8c42246d1af680e3adcd4f53e47b2bf2ddc65161
Juan david <juandavidx40@gmail.com>
2025-09-28 01:25:00 -0500

    delete backend

--- src/test-db.js
+++
@@ -1,9 +0,0 @@
-app.get("/testdb", async (req, res) => {
-    try {
-        const [rows] = await pool.query("SELECT NOW() as now");
-        res.json(rows);
-    } catch (err) {
-        console.error("❌ Error conectando a la DB:", err);
-        res.status(500).json({ error: "No se pudo conectar a la DB" });
-    }
-});
