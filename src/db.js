c3c1bc9f1ef27a72dd81696b2e6d362944346e00
Juan david <juandavidx40@gmail.com>
2025-09-27 22:24:05 -0500

    newsss

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import {createPool} from "mysql2/promise"
-
-export const pool = createPool({
-    user: "root",
-    password: "jaja1193430070",
-    host: "localhost",
-    port : 3306,
-    database: "bite_back1"
-})
